a3bb3731d8494a70da6f734449abc659fcb36c99
OscarSilvaOfficial <OscarSilvaOfficial>
2022-04-14 02:01:14 -0300

    Ajuste no nome da factory

--- app/main/factories/email_sender.py
+++ app/main/factories/email_sender.py
@@ -1,11 +1,8 @@
 from app.infra.configs.enviroment import ROOT_EMAIL, ROOT_PASSWORD
 from app.infra.services.mail import EmailCredentials, EmailSender, SMTPServerInfo
 
-class Factory:
-  
-  @staticmethod
-  def email_sender():
-    return EmailSender(
-      credentials=EmailCredentials(ROOT_EMAIL, ROOT_PASSWORD), 
-      smtp=SMTPServerInfo()
-    )
+def email_sender_factory():
+  return EmailSender(
+    credentials=EmailCredentials(ROOT_EMAIL, ROOT_PASSWORD), 
+    smtp=SMTPServerInfo()
+  )
--- app/main/routes/contacts.py
+++ app/main/routes/contacts.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter
 from app.adapters.controllers.contact_controller import ContactController
-from app.main.factories.email_sender import Factory
+from app.main.factories.email_sender import email_sender_factory
 from app.models.contact import Contact
 
 router = APIRouter(prefix="/contacts")
-controller = ContactController(contact_service=Factory.email_sender())
+controller = ContactController(contact_service=email_sender_factory())
 
 @router.get("")
 async def root():
