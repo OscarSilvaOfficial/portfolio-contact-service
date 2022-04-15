2d750c57296e70cbec26672d9cb1fc5174ca1b47
OscarSilvaOfficial <OscarSilvaOfficial>
2022-04-15 15:40:10 -0300

    Ajuste no nome da função

--- app/adapters/controllers/contact_controller.py
+++ app/adapters/controllers/contact_controller.py
@@ -1,4 +1,4 @@
-from app.core.useCases import notify_contact_use_case, save_contact_use_case
+from app.core.useCases import notify_new_contact_use_case, save_contact_use_case
 from app.ports.contact_repository_contract import ContactRepositoryContract
 from app.ports.email_sender_contract import EmailSenderContract
 from app.core.models.contact import Contact
@@ -13,5 +13,5 @@ class ContactController:
     return { "message": "Api UP" }
 
   def contact_service(self, contact: Contact):
-    notify_contact_use_case(self.__email_sender_service, contact)
+    notify_new_contact_use_case(self.__email_sender_service, contact)
     return save_contact_use_case(self.__contact_repository, contact)
--- app/core/useCases/notify_contact_use_case.py
+++ app/core/useCases/notify_contact_use_case.py
@@ -2,7 +2,7 @@ from app.core.models.contact import Contact
 from app.ports.email_sender_contract import EmailSenderContract
 
 
-def notify_contact_use_case(email_sender_service: EmailSenderContract, contact: Contact):
+def notify_new_contact_use_case(email_sender_service: EmailSenderContract, contact: Contact):
   subject = 'Contato Portfólio'
   message = f'Nome: {contact.name}\nEmail: {contact.email}\nMensagem: {contact.message}'
   return email_sender_service.send(subject, message)
