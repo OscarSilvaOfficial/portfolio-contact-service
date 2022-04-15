b1671e070de892fbfbeffaea31ffc54603dd4a55
OscarSilvaOfficial <OscarSilvaOfficial>
2022-04-15 13:10:33 -0300

    Add valores default

--- app/infra/configs/enviroment.py
+++ app/infra/configs/enviroment.py
@@ -8,11 +8,11 @@ ROOT_EMAIL=os.environ.get('EMAIL')
 ROOT_PASSWORD=os.environ.get('PASSWORD')
 
 # Credenciais de acesso ao banco de dados DYNAMO
-DYNAMO_TABLE_NAME=os.environ.get('DYNAMO_TABLE_NAME')
-DYNAMO_HOST=os.environ.get('DYNAMO_HOST')
-DYNAMO_PORT=os.environ.get('DYNAMO_PORT')
+DYNAMO_TABLE_NAME=os.environ.get('DYNAMO_TABLE_NAME', 'contacts')
+DYNAMO_HOST=os.environ.get('DYNAMO_HOST', 'http://localhost')
+DYNAMO_PORT=os.environ.get('DYNAMO_PORT', '8888')
 
 # Credenciais de acesso ao banco de dados MONGO
-MONGO_CONNECTION_STR=os.environ.get('MONGO_CONNECTION_STR')
-MONGO_DB_NAME=os.environ.get('MONGO_DB_NAME')
-MONGO_DB_COLLECTION=os.environ.get('MONGO_DB_COLLECTION')
+MONGO_CONNECTION_STR=os.environ.get('MONGO_CONNECTION_STR', 'mongodb://root:root@localhost:27017/')
+MONGO_DB_NAME=os.environ.get('MONGO_DB_NAME', 'portfolio')
+MONGO_DB_COLLECTION=os.environ.get('MONGO_DB_COLLECTION', 'contacts')
